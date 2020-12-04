702a51f629c09c9a15b722d22d2271d6085674fa
iljapavlovs <ilja.pavlovs@neotech.lv>
2020-12-04 12:58:47 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "io.iljapavlovs"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.3")
+}
